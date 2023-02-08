b39b64b91009f5c2f54991fe77dc0b522d6d1477
kongsanghyean <dodoi77@naver.com>
2023-02-08 22:23:19 +0900

    Devloper 뉴스를 DB에 저장합니다.

--- src/crawler/crawler.module.ts
+++ src/crawler/crawler.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NewsLetter } from 'src/newsletter/entities/newsletter.entity';
 import { CrawlerController } from './crawler.controller';
 import { CrawlerService } from './crawler.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([NewsLetter])],
   controllers: [CrawlerController],
   providers: [CrawlerService],
   exports: [CrawlerService],

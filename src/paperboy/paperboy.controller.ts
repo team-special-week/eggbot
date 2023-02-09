7406aba544d0e9782028194edc314950663f5f64
Ballbot <5252bb@daum.net>
2023-02-09 21:48:37 +0900

    paperyboy 가 뉴스레터를 보내도록 개선

--- src/paperboy/paperboy.controller.ts
+++ src/paperboy/paperboy.controller.ts
@@ -1,14 +1,13 @@
 import { Controller } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
+import { Cron, Interval } from '@nestjs/schedule';
 import { PaperboyService } from './paperboy.service';
 
 @Controller('paperboy')
 export class PaperboyController {
   constructor(private readonly paperboyService: PaperboyService) {}
 
-  @Cron('0 30 09 * * *')
-  // @Cron('* * * * * *')
-  async paperboyScheduleAtMorning() {
+  @Cron('0 0 10,13,16,19 * * *')
+  async paperboySchedule() {
     await this.paperboyService.deliveryAllSubscribes();
   }
 }

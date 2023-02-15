d3dc693297e9331877d4f6dffc1b7e3542d52a36
YH CHO <iuncehiro@gmail.com>
2023-02-15 22:18:00 +0900

    Revert ":sparkles: 닷넷데브 사이트 파서 추가"

--- src/crawler/crawler.controller.ts
+++ src/crawler/crawler.controller.ts
@@ -1,13 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { GeekNewsCrawlerService } from './geeknews/geeknews-crawler.service';
-import { DotNetDevCrawlerService } from './dotnetdev/dotnetdev-crawler.service';
 
 @Controller('crawler')
 export class CrawlerController {
   constructor(
     private readonly geekNewsCrawlerService: GeekNewsCrawlerService,
-    private readonly dotNetDevCrawlerService: DotNetDevCrawlerService,
   ) {}
 
   @Cron('0 00 06 * * *')
@@ -18,10 +16,5 @@ export class CrawlerController {
   @Get('/geeknews')
   testGeekNewsCrawler() {
     return this.geekNewsCrawlerService.crawling();
-  }
-
-  @Get('/dotnetdev')
-  testDotNetDevCrawler() {
-    return this.dotNetDevCrawlerService.crawling();
   }
 }
--- src/crawler/crawler.module.ts
+++ src/crawler/crawler.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { NewsletterModule } from 'src/newsletter/newsletter.module';
 import { CrawlerController } from './crawler.controller';
 import { GeekNewsCrawlerService } from './geeknews/geeknews-crawler.service';
-import { DotNetDevCrawlerService } from './dotnetdev/dotnetdev-crawler.service';
 
 @Module({
   imports: [NewsletterModule],
   controllers: [CrawlerController],
-  providers: [GeekNewsCrawlerService, DotNetDevCrawlerService],
+  providers: [GeekNewsCrawlerService],
 })
 export class CrawlerModule {}

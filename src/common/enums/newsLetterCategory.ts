0fb8a0b660c1d77c7d1257d548f9f8a965be4ebb
Ballbot <5252bb@daum.net>
2023-02-19 10:54:17 +0900

    :fire: 안쓰는 카테고리리는 제거

--- src/common/enums/newsLetterCategory.ts
+++ src/common/enums/newsLetterCategory.ts
@@ -1,6 +1,5 @@
 export enum ENewsLetterCategory {
   DEVELOPER = 'DEVELOPER',
-  OVERWATCH2 = 'OVERWATCH2',
 }
 
 export const NewsLetterCategoryDropdown = [
@@ -8,11 +7,6 @@ export const NewsLetterCategoryDropdown = [
     label: 'IT/개발',
     description: 'IT/개발 관련 뉴스를 구독합니다.',
     value: ENewsLetterCategory.DEVELOPER,
-  },
-  {
-    label: '오버워치2',
-    description: '오버워치2 관련 뉴스를 구독합니다.',
-    value: ENewsLetterCategory.OVERWATCH2,
   },
 ] as INewsLetterCategoryDropdown[];
 
